8e87c3ca81a0cf627940828bf3f4b16a5b8aa5fe
Selçuk ÇUKUR <xLAZx@users.noreply.github.com>
2013-12-14 20:59:46 +0200

    Lua version 5.2.3 Mantıksal hatalar giderildi.

--- src/scripting/Lua/lmem.c
+++ src/scripting/Lua/lmem.c
@@ -40,7 +40,7 @@
 
 
 
-#define MINSIZEARRAY	4
+#define MINSIZEARRAY        4
 
 
 void *luaM_growaux_ (lua_State *L, void *block, int *size, size_t size_elems,
@@ -96,4 +96,3 @@ void *luaM_realloc_ (lua_State *L, void *block, size_t osize, size_t nsize) {
   g->GCdebt = (g->GCdebt + nsize) - realosize;
   return newblock;
 }
-
