8020a3527fafb883ab3b7a1b08b87251b63cdad2
Selçuk ÇUKUR <xLAZx@users.noreply.github.com>
2013-12-14 23:11:07 +0200

    Log Yapısı, LUA Yapısında Değişiklik v.b. Detaylar Açıklamada…
    * DeathLog Eklendi, NPC veya User Tarafından Ölümleri Loglar, Party
    Bilgilerinide Yazar...
    * DeathNpc Eklendi, Kesilen NPC/Monster Bilgilerini Loglar.
    * LoginUser Eklendi, Login Server Loglarını Tutar.
    * ChatLog Eklendi, All Chat,Shout,Private Chat,Clan Chat, Alliance Chat,
    King Chat, Captain Chat, GM Command Tamamını Anlaşılır Şekilde Loglar...

--- src/GameServer/main.cpp
+++ src/GameServer/main.cpp
@@ -14,63 +14,72 @@ bool g_bRunning = true;
 
 int main()
 {
-	SetConsoleTitle("Game Server for Knight Online v" STRINGIFY(__VERSION));
+        SetConsoleTitle("Game Server for Knight Online v" STRINGIFY(__VERSION));
 
 #ifdef WIN32
-	// Override the console handler
-	SetConsoleCtrlHandler(_ConsoleHandler, TRUE);
+        // Override the console handler
+        SetConsoleCtrlHandler(_ConsoleHandler, TRUE);
 #endif
 
-	HookSignals(&s_hEvent);
+        HookSignals(&s_hEvent);
 
-	// Start up the time updater thread
-	StartTimeThread();
+        // Start up the time updater thread
+        StartTimeThread();
 
-	// Start up the console input thread
-	StartConsoleInputThread();
+        // Start up the console input thread
+        StartConsoleInputThread();
 
-	g_pMain = new CGameServerDlg();
+        g_pMain = new CGameServerDlg();
 
-	// Start up server
-	if (g_pMain->Startup())
-	{
-		// Reset Battle Zone Variables.
-		g_pMain->ResetBattleZone();
+        // Start up server
+        if (g_pMain->Startup())
+        {
+                // Reset Battle Zone Variables.
+                g_pMain->ResetBattleZone();
 
-		printf("\nServer started up successfully!\n");
-		
-		// Wait until console's signaled as closing
-		s_hEvent.Wait();
-	}
-	else
-	{
+                printf("\nServer started up successfully!\n");
+
+                // Wait until console's signaled as closing
+                s_hEvent.Wait();
+        }
+        else
+        {
 #ifdef WIN32
-		system("pause");
+                system("pause");
 #endif
-	}
+        }
 
-	printf("Server shutting down, please wait...\n");
+        printf("Server shutting down, please wait...\n");
 
-	// This seems redundant, but it's not. 
-	// We still have the destructor for the dialog instance, which allows time for threads to properly cleanup.
-	g_bRunning = false; 
+        if (g_pMain->m_fpDeathUser != nullptr)
+                fclose(g_pMain->m_fpDeathUser);
 
-	delete g_pMain;
+        if (g_pMain->m_fpDeathNpc != nullptr)
+                fclose(g_pMain->m_fpDeathNpc);
 
-	CleanupTimeThread();
-	CleanupConsoleInputThread();
-	UnhookSignals();
+        if (g_pMain->m_fpChat != nullptr)
+                fclose(g_pMain->m_fpChat);
 
-	return 0;
+        // This seems redundant, but it's not. 
+        // We still have the destructor for the dialog instance, which allows time for threads to properly cleanup.
+        g_bRunning = false; 
+
+        delete g_pMain;
+
+        CleanupTimeThread();
+        CleanupConsoleInputThread();
+        UnhookSignals();
+
+        return 0;
 }
 
 #ifdef WIN32
 BOOL WINAPI _ConsoleHandler(DWORD dwCtrlType)
 {
-	s_hEvent.BeginSynchronized();
-	s_hEvent.Signal();
-	s_hEvent.EndSynchronized();
-	sleep(10000); // Win7 onwards allows 10 seconds before it'll forcibly terminate
-	return TRUE;
+        s_hEvent.BeginSynchronized();
+        s_hEvent.Signal();
+        s_hEvent.EndSynchronized();
+        sleep(10000); // Win7 onwards allows 10 seconds before it'll forcibly terminate
+        return TRUE;
 }
 #endif
